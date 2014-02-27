555e9acaf2f8293914745078d016d73c9cb38258
roman <roman@netrga.es>
2014-02-27 13:56:21 +0100

    si no existe la plantilla no la copia

--- registropaus-repo/src/main/amp/config/alfresco/module/registropaus-repo/scripts/nuevoPAU.js
+++ registropaus-repo/src/main/amp/config/alfresco/module/registropaus-repo/scripts/nuevoPAU.js
@@ -1,9 +1,6 @@
 /*
  * nuevoSitio.js
  */
-//ejemplo: http://forums.alfresco.com/forum/end-user-discussions/alfresco-share/creation-site-space-template-02232012-1648
-// execute onCreate of a Site
-//var site = document;
 var childAssoc = behaviour.args[0];
 var pau = childAssoc.getChild();
 
@@ -16,25 +13,16 @@ logger.log("### Prepping to get Templates folder");
 
 var templates = search.luceneSearch('PATH:"/app:company_home/app:dictionary/app:space_templates/cm:PAU_Plantilla"');
 
-//var docLib = pau.childByNamePath("documentLibrary");
 
-//if (!docLib) {
+var pautemp = templates[0];
 
-//	docLib = pau.createFolder("documentLibrary");
 
-//}
+if(pautemp) {
+	logger.log("Plantilla encontrada:"+pautemp.children[0].name);
 
-//if (docLib) {
-
-	var tecoremtemp = templates[0];
-
-	// var child = templates.children[0];
-
-	logger.log("Plantilla encontrada:"+tecoremtemp.children[0].name);
-
-	for (t in tecoremtemp.children) {
-		logger.log("-" + tecoremtemp.children[t].name);
-		var plant = tecoremtemp.children[t];
+	for (t in pautemp.children) {
+		logger.log("-" + pautemp.children[t].name);
+		var plant = pautemp.children[t];
 
 		plant.copy(pau, true);
 
@@ -43,5 +31,4 @@ var templates = search.luceneSearch('PATH:"/app:company_home/app:dictionary/app:
 		}
 
 	}
-
-//} // end if
+}
